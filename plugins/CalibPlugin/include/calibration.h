c75cbabafa84c5ace0876070534439f4f4189c43
introvertie <x523356708@gmail.com>
2022-08-10 16:40:56 +0800

    relative debug addr

--- plugins/CalibPlugin/include/calibration.h
+++ plugins/CalibPlugin/include/calibration.h
@@ -29,99 +29,110 @@ typedef pcl::PointCloud<pcl::PointXYZI>::Ptr CloudPtr;
 typedef Eigen::Matrix4f RTmatrix;
 
 struct Ballot {
-  Ballot() : x(0), y(0), z(0), angle(0){};
-  float x;
-  float y;
-  float z;
-  float angle;
+    Ballot() : x(0), y(0), z(0), angle(0){};
+    float x;
+    float y;
+    float z;
+    float angle;
 };
 
 class Operator {
- public:
-  enum CalibrationError {
-    CE_INPUT_NULL = 1,
-    CE_ATTENSION_NULL = 2,
-    CE_CLUSTER_FAIL = 3,
-    CE_NO_RESULT = 4,
-  };
-  enum LidarModel {
-    JAGUAR = 1,
-    FALCON = 2,
-  };
-  Operator() {
-    tree_.reset(new pcl::search::KdTree<pcl::PointXYZI>());
-    kdtree_.reset((new pcl::KdTreeFLANN<pcl::PointXYZI>));
-    normal_estimator_.setSearchMethod(tree_);
-    normal_estimator_.setKSearch(50);
-    smoothness_ = 20.0;
-    curvature_ = 0.2;
-    save_flag_ = true;
-    lidar_model_ = FALCON;
-  }
-  ~Operator() {}
-
-  void findAround(CloudPtr source, CloudPtr &around, CloudPtr &attention,
-                  const float &intensity);
-
-  void guide_filter(CloudPtr source, CloudPtr &result);
-  void dbscan(CloudPtr source, CloudPtr &result);
-  void euclidean_cluster(CloudPtr source, CloudPtr &result);
-  void region_growing_cluster(CloudPtr source, CloudPtr &result,
-                              const bool &keep_all);
-  void outline_remove(CloudPtr source, CloudPtr &result);
-
-  void voteNormal(CloudPtr source, CloudPtr &result, float &angle);
-  void voteDistance(CloudPtr reference, CloudPtr target, CloudPtr &result);
-
-  void fitPlane(CloudPtr source, CloudPtr &result, float &a, float &b, float &c,
-                float &d, const double &threshold);
-  void fitPlane(CloudPtr source, CloudPtr &result, const double &threshold);
-  void fitCircle(CloudPtr source, float &center_x, float &center_y,
-                 float &center_z, float &radius);
-  void findPlate(CloudPtr source, CloudPtr &result,
-                 const pcl::Normal &reference);
-  void mergePointCloud(CloudPtr source, CloudPtr &result);
-  void nearMarkable(CloudPtr source, CloudPtr &result);
-
-  Eigen::Vector3f find_feature_point(CloudPtr source);
-
-  int compute(Eigen::Vector3f &feature_point, CloudPtr &output);
-
-  // Settings
-  inline void setFrameName(const std::string &name) { frame_name_ = name; }
-  inline void setDebugFolder(const std::string &name) { debug_folder_ = name; }
-  inline void setInputCloud(const CloudPtr &input) { input_cloud_ = input; }
-  inline void setSaveFlag(const bool &flag) { save_flag_ = flag; }
-  inline void setLidarModel(const int &model) {
-    lidar_model_ = LidarModel(model);
-    if (lidar_model_ == JAGUAR) {
-      // Jaguar 6.0 , 0.05
-      smoothness_ = 6.0;
-      curvature_ = 0.05;
+   public:
+    enum CalibrationError {
+        CE_INPUT_NULL = 1,
+        CE_ATTENSION_NULL = 2,
+        CE_CLUSTER_FAIL = 3,
+        CE_NO_RESULT = 4,
+    };
+    enum LidarModel {
+        JAGUAR = 1,
+        FALCON = 2,
+    };
+    Operator() {
+        tree_.reset(new pcl::search::KdTree<pcl::PointXYZI>());
+        kdtree_.reset((new pcl::KdTreeFLANN<pcl::PointXYZI>));
+        normal_estimator_.setSearchMethod(tree_);
+        normal_estimator_.setKSearch(50);
+        smoothness_ = 20.0;
+        curvature_ = 0.2;
+        save_flag_ = true;
+        lidar_model_ = FALCON;
     }
-    if (lidar_model_ == FALCON) {
-      // Jaguar 20.0 , 0.2
-      smoothness_ = 20.0;
-      curvature_ = 0.2;
-    }
-  }
-  void save_pcd(std::string name, const CloudPtr &cloud);
+    ~Operator() {}
 
- private:
-  float a_;
-  float b_;
-  float c_;
-  float d_;
-  float smoothness_;
-  float curvature_;
-  bool save_flag_;
-  LidarModel lidar_model_;
-  std::string frame_name_;
-  std::string debug_folder_;
-  CloudPtr input_cloud_;  // 完整的输入点云
-  pcl::search::KdTree<pcl::PointXYZI>::Ptr tree_;
-  pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr kdtree_;
-  pcl::NormalEstimation<pcl::PointXYZI, pcl::Normal> normal_estimator_;
+    void findAround(CloudPtr source, CloudPtr &around, CloudPtr &attention,
+                    const float &intensity);
+
+    void guide_filter(CloudPtr source, CloudPtr &result);
+    void dbscan(CloudPtr source, CloudPtr &result);
+    void euclidean_cluster(CloudPtr source, CloudPtr &result);
+    void region_growing_cluster(CloudPtr source, CloudPtr &result,
+                                const bool &keep_all);
+    void outline_remove(CloudPtr source, CloudPtr &result);
+
+    void voteNormal(CloudPtr source, CloudPtr &result, float &angle);
+    void voteDistance(CloudPtr reference, CloudPtr target, CloudPtr &result);
+
+    void fitPlane(CloudPtr source, CloudPtr &result, float &a, float &b,
+                  float &c, float &d, const double &threshold);
+    void fitPlane(CloudPtr source, CloudPtr &result, const double &threshold);
+    void fitCircle(CloudPtr source, float &center_x, float &center_y,
+                   float &center_z, float &radius);
+    void findPlate(CloudPtr source, CloudPtr &result,
+                   const pcl::Normal &reference);
+    void mergePointCloud(CloudPtr source, CloudPtr &result);
+    void nearMarkable(CloudPtr source, CloudPtr &result);
+
+    Eigen::Vector3f find_feature_point(CloudPtr source);
+
+    int compute(Eigen::Vector3f &feature_point, CloudPtr &output);
+
+    // Settings
+    inline void setFrameName(const std::string &name) { frame_name_ = name; }
+    inline void setDebugFolder(const std::string &name) {
+        debug_folder_ = name;
+        if (debug_folder_.size()) {
+            if (debug_folder_.substr(debug_folder_.size() - 1, 1) !=
+                std::string("/")) {
+                debug_folder_ = debug_folder_ + "/";
+            }
+            save_flag_ = true;
+        } else {
+            save_flag_ = false;
+        }
+    }
+    inline void setInputCloud(const CloudPtr &input) { input_cloud_ = input; }
+    inline void setSaveFlag(const bool &flag) { save_flag_ = flag; }
+    inline void setLidarModel(const int &model) {
+        lidar_model_ = LidarModel(model);
+        if (lidar_model_ == JAGUAR) {
+            // Jaguar 6.0 , 0.05
+            smoothness_ = 6.0;
+            curvature_ = 0.05;
+        }
+        if (lidar_model_ == FALCON) {
+            // Jaguar 20.0 , 0.2
+            smoothness_ = 20.0;
+            curvature_ = 0.2;
+        }
+    }
+    void save_pcd(std::string name, const CloudPtr &cloud);
+
+   private:
+    float a_;
+    float b_;
+    float c_;
+    float d_;
+    float smoothness_;
+    float curvature_;
+    bool save_flag_;
+    LidarModel lidar_model_;
+    std::string frame_name_;
+    std::string debug_folder_;
+    CloudPtr input_cloud_;  // 完整的输入点云
+    pcl::search::KdTree<pcl::PointXYZI>::Ptr tree_;
+    pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr kdtree_;
+    pcl::NormalEstimation<pcl::PointXYZI, pcl::Normal> normal_estimator_;
 };
 Eigen::Matrix3f transforme_normal_normal_3d(const Eigen::Vector3f &start,
                                             const Eigen::Vector3f &final);
--- plugins/CalibPlugin/src/calibplugin.cpp
+++ plugins/CalibPlugin/src/calibplugin.cpp
@@ -50,14 +50,14 @@
 #include "sm2cc.h"
 
 QString parse_vector3f(const Eigen::Vector3f &vec) {
-  std::string res = "[x,y,z] = [";
-  res += std::to_string(vec(0));
-  res += ",";
-  res += std::to_string(vec(1));
-  res += ",";
-  res += std::to_string(vec(2));
-  res += "]";
-  return QString::fromStdString(res);
+    std::string res = "[x,y,z] = [";
+    res += std::to_string(vec(0));
+    res += ",";
+    res += std::to_string(vec(1));
+    res += ",";
+    res += std::to_string(vec(2));
+    res += "]";
+    return QString::fromStdString(res);
 }
 // Default constructor:
 //	- pass the Qt resource path to the info.json file (from
@@ -72,176 +72,193 @@ CalibPlugin::CalibPlugin(QObject *parent)
 // This method should enable or disable your plugin actions
 // depending on the currently selected entities ('selectedEntities').
 void CalibPlugin::onNewSelection(const ccHObject::Container &selectedEntities) {
-  if (m_action == nullptr) {
-    return;
-  }
+    if (m_action == nullptr) {
+        return;
+    }
 
-  // If you need to check for a specific type of object, you can use the methods
-  // in ccHObjectCaster.h or loop and check the objects' classIDs like this:
-  //
-  //	for ( ccHObject *object : selectedEntities )
-  //	{
-  //		if ( object->getClassID() == CC_TYPES::VIEWPORT_2D_OBJECT )
-  //		{
-  //			// ... do something with the viewports
-  //		}
-  //	}
+    // If you need to check for a specific type of object, you can use the
+    // methods in ccHObjectCaster.h or loop and check the objects' classIDs like
+    // this:
+    //
+    //	for ( ccHObject *object : selectedEntities )
+    //	{
+    //		if ( object->getClassID() == CC_TYPES::VIEWPORT_2D_OBJECT )
+    //		{
+    //			// ... do something with the viewports
+    //		}
+    //	}
 
-  // For example - only enable our action if something is selected.
-  m_action->setEnabled(!selectedEntities.empty());
+    // For example - only enable our action if something is selected.
+    m_action->setEnabled(!selectedEntities.empty());
 }
 
 // This method returns all the 'actions' your plugin can perform.
 // getActions() will be called only once, when plugin is loaded.
 QList<QAction *> CalibPlugin::getActions() {
-  // default action (if it has not been already created, this is the moment to
-  // do it)
-  if (!m_action) {
-    // Here we use the default plugin name, description, and icon,
-    // but each action should have its own.
-    m_action = new QAction(getName(), this);
-    m_action->setToolTip(getDescription());
-    m_action->setIcon(getIcon());
+    // default action (if it has not been already created, this is the moment to
+    // do it)
+    if (!m_action) {
+        // Here we use the default plugin name, description, and icon,
+        // but each action should have its own.
+        m_action = new QAction(getName(), this);
+        m_action->setToolTip(getDescription());
+        m_action->setIcon(getIcon());
 
-    // Connect appropriate signal
-    connect(m_action, &QAction::triggered, this,
-            [this]() { this->doAction(); });
-  }
+        // Connect appropriate signal
+        connect(m_action, &QAction::triggered, this,
+                [this]() { this->doAction(); });
+    }
 
-  return {m_action};
+    return {m_action};
 }
 
 void CalibPlugin::doAction() {
-  // null app
-  if (m_app == nullptr) {
-    Q_ASSERT(false);
-    return;
-  }
-  // fail to launch qwidget
-  CalibDlg dlg(m_app->getMainWindow());
-  if (!dlg.exec()) {
-    return;
-  }
-  // check selected element number
-  const ccHObject::Container &selectedEntities = m_app->getSelectedEntities();
-  // this case should never occur
-  if (selectedEntities.size() < 1) {
-    m_app->dispToConsole("[Calibration] Please select at least one pointcloud",
-                         ccMainAppInterface::ERR_CONSOLE_MESSAGE);
-    return;
-  }
-  // check the destination file address
-  const QString qs_addr = dlg.openFilesPath;
-  const std::string file_addr = qs_addr.toStdString();
-  const QString defaultSFName = dlg.lidar_field_box->currentText();
-  const QString lidar_model = dlg.lidar_type_box->currentText();
-  if (lidar_model == "Jaguar") {
-    m_operator->setLidarModel(1);
-  } else if (lidar_model == "Falcon") {
-    m_operator->setLidarModel(2);
-  }
-  std::fstream fd(file_addr, std::ios::out);
-  if (!fd.is_open()) {
-    m_app->dispToConsole(
-        QString("[Calibration] %1 : Not a valid path.").arg(qs_addr),
-        ccMainAppInterface::ERR_CONSOLE_MESSAGE);
-    return;
-  }
-  // header
-  fd << "[lidar]" << std::endl;
-  // main process
-  for (size_t i = 0; i < selectedEntities.size(); ++i) {
-    ccPointCloud *cloud;
-    ccHObject *entity = selectedEntities[i];
-    // check entity type
-    if (entity->isA(CC_TYPES::POINT_CLOUD)) {
-      cloud = static_cast<ccPointCloud *>(entity);
-      int sfIdx = -1;
-      // check ScalarFields
-      if (cloud->getNumberOfScalarFields() >= 1) {
-        sfIdx = cloud->getScalarFieldIndexByName(qPrintable(defaultSFName));
-        // check ScalarFiled offset
-        if (sfIdx >= 0) {
-          CCCoreLib::ScalarField *sf = cloud->getScalarField(sfIdx);
-          pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud(
-              new pcl::PointCloud<pcl::PointXYZI>);
-          pcl::PointCloud<pcl::PointXYZI>::Ptr output_cloud(
-              new pcl::PointCloud<pcl::PointXYZI>);
-          pcl::PCLPointCloud2::Ptr output_cloud2(new pcl::PCLPointCloud2);
-          for (unsigned int ii = 0; ii < cloud->size(); ++ii) {
-            auto t_src = cloud->getPoint(ii);
-            pcl::PointXYZI t_point{t_src->x, t_src->y, t_src->z};
-            t_point.intensity = float(sf->getValue(ii));
-            input_cloud->emplace_back(t_point);
-          }
-          Eigen::Vector3f feature_point;
-          m_operator->setInputCloud(input_cloud);
-          m_operator->setFrameName(std::to_string(i));
-          m_operator->setDebugFolder("/home/xavier/old/0704/debug/");
-          int comput_status = m_operator->compute(feature_point, output_cloud);
-          if (comput_status == 0) {
-            m_app->dispToConsole(QString("[Calibration] Output Cloud Size %1")
-                                     .arg(output_cloud->size()),
-                                 ccMainAppInterface::STD_CONSOLE_MESSAGE);
-            pcl::toPCLPointCloud2(*output_cloud, *output_cloud2);
-            auto cc_cloud_ptr = pcl2cc::Convert(*output_cloud2);
-            if (cc_cloud_ptr) {
-              cc_cloud_ptr->setName(cloud->getName() + QString(".Calibration"));
-              cc_cloud_ptr->setGlobalShift(cloud->getGlobalShift());
-              cc_cloud_ptr->setGlobalScale(cloud->getGlobalScale());
-              cc_cloud_ptr->setDisplay(cloud->getDisplay());
-              cc_cloud_ptr->prepareDisplayForRefresh();
-              if (cloud->getParent())
-                cloud->getParent()->addChild(cc_cloud_ptr);
-              cloud->setEnabled(false);
-              m_app->addToDB(cc_cloud_ptr);
-              cc_cloud_ptr->prepareDisplayForRefresh();
-              m_app->dispToConsole(
-                  QString("[Calibration] Get center point coordinate %1")
-                      .arg(parse_vector3f(feature_point)),
-                  ccMainAppInterface::STD_CONSOLE_MESSAGE);
-              fd << feature_point(0) << "," << feature_point(1) << ","
-                 << feature_point(2);
-            } else {
-              m_app->dispToConsole(
-                  QString("[Calibration] Entity[%1] ccPointCloud is nullptr.")
-                      .arg(i),
-                  ccMainAppInterface::ERR_CONSOLE_MESSAGE);
-            }
-          } else {
-            m_app->dispToConsole(
-                QString(
-                    "[Calibration] Entity[%1] Failed to find feature point.")
-                    .arg(i),
-                ccMainAppInterface::ERR_CONSOLE_MESSAGE);
-            m_app->dispToConsole(
-                QString("[Calibration] ErrorCode :%1 .").arg(comput_status),
-                ccMainAppInterface::ERR_CONSOLE_MESSAGE);
-          }
-        } else {
-          m_app->dispToConsole(
-              QString(
-                  "[Calibration] Entity[%1] ScalarFiled offset is not valid.")
-                  .arg(i),
-              ccMainAppInterface::ERR_CONSOLE_MESSAGE);
-        }
-      } else {
-        m_app->dispToConsole(
-            QString("[Calibration] Entity[%1] doesnt have ScalarFields.")
-                .arg(i),
-            ccMainAppInterface::ERR_CONSOLE_MESSAGE);
-      }
-    } else {
-      m_app->dispToConsole(
-          QString("[Calibration] Entity[%1] type is not CC_TYPES::POINT_CLOUD.")
-              .arg(i),
-          ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+    // null app
+    if (m_app == nullptr) {
+        Q_ASSERT(false);
+        return;
     }
-    fd << std::endl;
-  }
-  fd.close();
-  m_app->dispToConsole(
-      QString("[Calibration] Save feature point coordinate to :  %1")
-          .arg(qs_addr),
-      ccMainAppInterface::STD_CONSOLE_MESSAGE);
+    // fail to launch qwidget
+    CalibDlg dlg(m_app->getMainWindow());
+    if (!dlg.exec()) {
+        return;
+    }
+    // check selected element number
+    const ccHObject::Container &selectedEntities = m_app->getSelectedEntities();
+    // this case should never occur
+    if (selectedEntities.size() < 1) {
+        m_app->dispToConsole(
+            "[Calibration] Please select at least one pointcloud",
+            ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+        return;
+    }
+    // check the destination file address
+    const QString qs_addr = dlg.openFilesPath;
+    const std::string file_addr = qs_addr.toStdString();
+    const QString defaultSFName = dlg.lidar_field_box->currentText();
+    const QString lidar_model = dlg.lidar_type_box->currentText();
+    if (lidar_model == "Jaguar") {
+        m_operator->setLidarModel(1);
+    } else if (lidar_model == "Falcon") {
+        m_operator->setLidarModel(2);
+    }
+    std::fstream fd(file_addr, std::ios::out);
+    if (!fd.is_open()) {
+        m_app->dispToConsole(
+            QString("[Calibration] %1 : Not a valid path.").arg(qs_addr),
+            ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+        return;
+    }
+    size_t end_ite = file_addr.find_last_of('/');
+    std::string debug_folder =
+        end_ite > 0 ? file_addr.substr(0, end_ite) : "";
+    std::cout << debug_folder << std::endl;
+    // header
+    fd << "[lidar]" << std::endl;
+    // main process
+    for (size_t i = 0; i < selectedEntities.size(); ++i) {
+        ccPointCloud *cloud;
+        ccHObject *entity = selectedEntities[i];
+        // check entity type
+        if (entity->isA(CC_TYPES::POINT_CLOUD)) {
+            cloud = static_cast<ccPointCloud *>(entity);
+            int sfIdx = -1;
+            // check ScalarFields
+            if (cloud->getNumberOfScalarFields() >= 1) {
+                sfIdx =
+                    cloud->getScalarFieldIndexByName(qPrintable(defaultSFName));
+                // check ScalarFiled offset
+                if (sfIdx >= 0) {
+                    CCCoreLib::ScalarField *sf = cloud->getScalarField(sfIdx);
+                    pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud(
+                        new pcl::PointCloud<pcl::PointXYZI>);
+                    pcl::PointCloud<pcl::PointXYZI>::Ptr output_cloud(
+                        new pcl::PointCloud<pcl::PointXYZI>);
+                    pcl::PCLPointCloud2::Ptr output_cloud2(
+                        new pcl::PCLPointCloud2);
+                    for (unsigned int ii = 0; ii < cloud->size(); ++ii) {
+                        auto t_src = cloud->getPoint(ii);
+                        pcl::PointXYZI t_point{t_src->x, t_src->y, t_src->z};
+                        t_point.intensity = float(sf->getValue(ii));
+                        input_cloud->emplace_back(t_point);
+                    }
+                    Eigen::Vector3f feature_point;
+                    m_operator->setInputCloud(input_cloud);
+                    m_operator->setFrameName(std::to_string(i));
+                    m_operator->setDebugFolder(debug_folder);
+                    int comput_status =
+                        m_operator->compute(feature_point, output_cloud);
+                    if (comput_status == 0) {
+                        m_app->dispToConsole(
+                            QString("[Calibration] Output Cloud Size %1")
+                                .arg(output_cloud->size()),
+                            ccMainAppInterface::STD_CONSOLE_MESSAGE);
+                        pcl::toPCLPointCloud2(*output_cloud, *output_cloud2);
+                        auto cc_cloud_ptr = pcl2cc::Convert(*output_cloud2);
+                        if (cc_cloud_ptr) {
+                            cc_cloud_ptr->setName(cloud->getName() +
+                                                  QString(".Calibration"));
+                            cc_cloud_ptr->setGlobalShift(
+                                cloud->getGlobalShift());
+                            cc_cloud_ptr->setGlobalScale(
+                                cloud->getGlobalScale());
+                            cc_cloud_ptr->setDisplay(cloud->getDisplay());
+                            cc_cloud_ptr->prepareDisplayForRefresh();
+                            if (cloud->getParent())
+                                cloud->getParent()->addChild(cc_cloud_ptr);
+                            cloud->setEnabled(false);
+                            m_app->addToDB(cc_cloud_ptr);
+                            cc_cloud_ptr->prepareDisplayForRefresh();
+                            m_app->dispToConsole(
+                                QString("[Calibration] Get center point "
+                                        "coordinate %1")
+                                    .arg(parse_vector3f(feature_point)),
+                                ccMainAppInterface::STD_CONSOLE_MESSAGE);
+                            fd << feature_point(0) << "," << feature_point(1)
+                               << "," << feature_point(2);
+                        } else {
+                            m_app->dispToConsole(
+                                QString("[Calibration] Entity[%1] ccPointCloud "
+                                        "is nullptr.")
+                                    .arg(i),
+                                ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+                        }
+                    } else {
+                        m_app->dispToConsole(
+                            QString("[Calibration] Entity[%1] Failed to find "
+                                    "feature point.")
+                                .arg(i),
+                            ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+                        m_app->dispToConsole(
+                            QString("[Calibration] ErrorCode :%1 .")
+                                .arg(comput_status),
+                            ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+                    }
+                } else {
+                    m_app->dispToConsole(
+                        QString("[Calibration] Entity[%1] ScalarFiled offset "
+                                "is not valid.")
+                            .arg(i),
+                        ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+                }
+            } else {
+                m_app->dispToConsole(
+                    QString(
+                        "[Calibration] Entity[%1] doesnt have ScalarFields.")
+                        .arg(i),
+                    ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+            }
+        } else {
+            m_app->dispToConsole(QString("[Calibration] Entity[%1] type is not "
+                                         "CC_TYPES::POINT_CLOUD.")
+                                     .arg(i),
+                                 ccMainAppInterface::ERR_CONSOLE_MESSAGE);
+        }
+        fd << std::endl;
+    }
+    fd.close();
+    m_app->dispToConsole(
+        QString("[Calibration] Save feature point coordinate to :  %1")
+            .arg(qs_addr),
+        ccMainAppInterface::STD_CONSOLE_MESSAGE);
 }
