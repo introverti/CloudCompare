8d27c30314fc49e2bb6b215cde730146a6448b68
introvertie <x523356708@gmail.com>
2022-08-23 17:09:33 +0800

    try to make snap

--- scripts/linux/snap_deploy.sh
+++ scripts/linux/snap_deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 WORKING_DIR="$1"
-
 docker run -t \
   -v "${WORKING_DIR}":"${WORKING_DIR}" \
   -e LC_ALL="C.UTF-8" \
   -e LANG="C.UTF-8" \
-  ubuntu:xenial \
-  sh -c "apt-get -qq update && apt-get -y install snapcraft && cd ${WORKING_DIR} && snapcraft && snapcraft push *.snap --release edge"
+  ubuntu:bionic \
+  sh -c "echo 'tzdata tzdata/Areas select Europe' | debconf-set-selections && echo 'tzdata tzdata/Zones/Europe select Paris' | debconf-set-selections && export DEBIAN_FRONTEND=noninteractive && apt-get -qq update && apt-get install -y tzdata && apt-get -y install snapcraft && cd ${WORKING_DIR} && snapcraft && snapcraft push *.snap --release edge"
